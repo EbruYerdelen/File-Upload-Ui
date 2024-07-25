d7326f8886a5e9e5e15d7d78f114015a37b19597
Ebru <yerdebru@gmail.com>
2024-07-25 17:08:26 +0300

    implement-truncated-file

--- src/FileName.tsx
+++ src/FileName.tsx
@@ -1,15 +1,18 @@
+import { truncateFileName } from "./utils";
+
 interface FileUploadProps {
   fileName: string;
 }
 
 const FileName = ({ fileName }: FileUploadProps) => {
+  const truncatedFileName = truncateFileName(fileName, 20);
   return (
     <>
       {fileName ? (
         <span
           className={`p-[5px] ml-3 font-medium text-[rgba(29,29,29,0.89)] text-md `}
         >
-          {fileName}
+          {truncatedFileName}
         </span>
       ) : (
         <span className="p-3 text-[rgba(102,101,101,0.77)] font-thin text-md ">
