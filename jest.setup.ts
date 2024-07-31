07b9fc68190d596cc2a8d15f5c577733113bcf32
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-07-31 18:27:31 -0300

    fix: Fixed jest related errors

---
+++ jest.setup.ts
@@ -0,0 +1,15 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    setupFilesAfterEnv:['<rootDir>/jest.setup.ts'],
+    transform: {
+      '^.+\\.(ts|tsx)$': ['ts-jest', {
+        tsconfig: 'tsconfig.app.json',
+      }],
+    },
+    moduleNameMapper: {
+      '^.+\\.svg$': 'jest-svg-transformer',
+      "\\.(css|less)$": "identity-obj-proxy",
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  };
