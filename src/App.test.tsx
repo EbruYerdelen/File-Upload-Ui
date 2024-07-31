fc938e955f68d1fd50fa50d28549ecce3de6438a
Ebru <yerdebru@gmail.com>
2024-08-01 00:09:17 +0300

    jest&test lib set up

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe(" Renders App", () => {
+  it("Renders App", () => {
+    render(<App/>);
+  });
+});
