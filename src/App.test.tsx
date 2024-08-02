592e23d6fca1db4b413167cf153c7799cf5723b9
Ebru <yerdebru@gmail.com>
2024-08-02 14:49:41 +0300

    fix import bug

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,17 @@
+/* @ts-ignore */
+import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe(App, () => {
-
   it("Check if initial button name updates,browse button name also updates.", () => {
     render(<App initialButtonName="Initial Name" />);
 
     // Check if the Browse button is rendered with the initial button name
     const initialButton = screen.getByText("Initial Name");
     expect(initialButton).toBeInTheDocument();
-  })
-
-
+  });
 
   it("Displays and renders the button containing XCircle icon when initial file name is provided", () => {
     render(<App initialFileName="example.txt" />);
@@ -20,8 +19,5 @@ describe(App, () => {
     // We are checking if the Browse button is rendered.
     const removeFileButton = screen.getByTestId("removeFile");
     expect(removeFileButton).toBeInTheDocument();
-
-
-  })
-    
+  });
 });
